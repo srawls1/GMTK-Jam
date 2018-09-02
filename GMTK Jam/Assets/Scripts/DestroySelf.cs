9386b014412bb37999a28e06565c3ca94f314163
Spencer Rawls <rawlsspencer@gmail.com>
2018-09-01 19:46:43 -0500

    Added in player blast assets

---
+++ GMTK Jam/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+    [SerializeField] private float duration;
+
+	void Start()
+    {
+		Destroy(gameObject, duration);
+	}
+}
